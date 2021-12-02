4f3e53fe6de4e856f1302cc377dcdbc5a3b09ab2
Alex BAKO <alex.bako@protonmail.com>
2021-12-02 20:15:31 +0100

    remove faulty gitops submodule

--- env-setup.sh
+++ env-setup.sh
@@ -11,3 +11,27 @@ ansible-playbook environment/k8s/setup.yaml --tags minikube
 
 # Setup Helm
 ansible-playbook environment/helm/setup.yaml
+
+# Update Minikube's IP address for JenkinsX
+cd environment/jenkins/jx-gitops
+export DOMAIN="$(minikube ip).nip.io"
+jx gitops requirements edit --domain $DOMAIN
+
+# Ngrok related stuff
+echo "|**************************************************************************************|"
+echo "| Now you need to start the ngrok with the following command:                          |"
+echo "|                                                                                      |"
+echo "|   ngrok http 8080                                                                    |"
+echo "|                                                                                      |"
+echo "| Once you've done that, you need to add your generated dns entry to the following     |"
+echo "| configuration file:                                                                  |"
+echo "|                                                                                      |"
+echo "| environment/jenkins/jx-gitops/charts/jenkins-x/jxboot-helmfile-resources/values.yaml |"
+echo "|                                                                                      |"
+echo "|**************************************************************************************|"
+
+# Install the GitOperator for JenkinsX
+jx admin operator \
+    --url=https://github.com/bak0al3x/jx-gitops \
+    --username bak0al3x \
+    --token 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
